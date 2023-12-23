808f40a27fe33f7e3de1aa59f79454c04f4fa019
Guilherme Henrique <112288334+guibr7@users.noreply.github.com>
2023-12-22 23:51:23 -0300

    atualizando projeto

--- 10projeto/front/app.js
+++ 10projeto/front/app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const expressHB = require('express-handlebars');
 const bodyParser = require('body-parser');
+const fetch = require('node-fetch');
 
 const app = express();
+
+//Especificar arquivos estáticos
+app.use(express.static(__dirname + '/public'))
 
 //Body-parser
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -15,9 +19,26 @@ app.set('view engine', 'handlebars');
 app.set('views', './views');
 
 app.get('/', (req, res) => {
-    res.render('inicio');
+    fetch('http://localhost:3000/clientes',{method:'GET'})
+    .then(resposta => resposta.json())
+    .then(resposta => res.render('inicio',{resposta}))
+    .catch(error => { res.status(500).send('Erro ao carregar dados');
+    });
 });
 
-app.listen(3000,()=>{
-    console.log('Servidor rodando em https://localhost:3000')
+app.post('/cadastrar',(req,res)=>{
+    let nome = req.body.nome;
+    let idade = req.body.idade;
+    let dados = {'nome':nome,'idade':idade};
+
+    fetch('http://localhost:3000/clientes',{method:'POST',
+        //A api entende euq é um texto, é preciso converter para json
+        body:JSON.stringify(dados) ,
+        headers:{'Content-Type':'application/json'}
+    })
+    .then(res.redirect('/'))
+});
+
+app.listen(8081,()=>{
+    console.log('Servidor rodando em https://localhost:8081')
 });
