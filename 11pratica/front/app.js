7529a417999296ac515f29f9921d59303e015f90
Guilherme Henrique <112288334+guibr7@users.noreply.github.com>
2024-01-03 09:41:10 -0300

    Praticando

--- 11pratica/front/app.js
+++ 11pratica/front/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const handlebars = require('express-handlebars')
 const fetch = require('node-fetch')
+const bodyParser = require('body-parser')
 const app = express()
 
 app.engine('handlebars', handlebars.engine({defaultLayout:'layout'}));
@@ -8,9 +9,35 @@ app.set('view engine', 'handlebars');
 app.set('views', './views');
 
 app.use(express.static(__dirname + '/public'))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 app.get('/',(req,res)=>{
-    res.render('inicio',{titulo:'Estoque'})
+    fetch('http://localhost:3000/estoque',{method:'GET'})
+    .then(resposta => resposta.json())
+    .then(resposta => res.render('inicio', { resposta,produtos:resposta, title: 'Estoque' }))
+   
+})
+
+app.get('/cadastrar',(req,res)=>{
+    res.render('cadastrar')
+})
+
+app.post('/cadastrarproduto',(req,res)=>{
+    let nome = req.body.nome;
+    let quant = req.body.quantidade;
+    let preco = req.body.preco
+    let produto = {
+        'nome':nome,
+        'quantidade':quant,
+        'preco':preco
+    };
+    fetch(' http://localhost:3000/estoque',{method:'POST',
+    body:JSON.stringify(produto),
+    headers:{'Content-Type':'application/json'}})
+    .then(res.redirect('/'))
+
+   
 })
 
 app.listen(8081,()=>{
