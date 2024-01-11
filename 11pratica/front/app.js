6271fbb11854f87b155382cc64aef71351868360
Guilherme Henrique <112288334+guibr7@users.noreply.github.com>
2024-01-11 19:57:35 -0300

    Praticando

--- 11pratica/front/app.js
+++ 11pratica/front/app.js
@@ -19,6 +19,7 @@ app.get('/',(req,res)=>{
    
 })
 
+
 app.get('/cadastrar',(req,res)=>{
     res.render('cadastrar')
 })
@@ -28,7 +29,7 @@ app.post('/cadastrarproduto',(req,res)=>{
     let quant = req.body.quantidade;
     let preco = req.body.preco
     let produto = {
-        'nome':nome,
+        'nome':nome[0].toUpperCase() + nome.substring(1),
         'quantidade':quant,
         'preco':preco
     };
@@ -39,6 +40,28 @@ app.post('/cadastrarproduto',(req,res)=>{
 
    
 })
+
+app.get('/selecionar/:id',(req,res)=>{
+    let id = req.params.id
+    fetch('http://localhost:3000/estoque/'+id,{method:'GET'})
+    .then(produto => produto.json())
+    .then(produto => res.render('selecionar',{produto,id}))
+})
+
+app.post('/editar', function(req, res){
+    let nome = req.body.nome;
+    let quantidade = req.body.quantidade;
+    let preco = req.body.preco;
+    let id = req.body.ident
+
+    fetch('http://localhost:3000/estoque/'+id, {
+        method:'PUT',
+        body:JSON.stringify({'nome':nome, 'quantidade':quantidade,'id':id,'preco':preco}),
+        headers:{'Content-Type':'application/json'}
+    })
+    .then(res.redirect('/'));
+});
+
 
 app.listen(8081,()=>{
     console.log('Servidor rodando em: http://localhost:8081')
